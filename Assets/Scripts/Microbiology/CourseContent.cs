2e22d3832112bcbe8996f68a33cd5b3c7efa62ca
Ante-237 <akumawahanye@gmail.com>
2023-06-13 03:28:33 +0200

    User Profile

---
+++ Assets/Scripts/Microbiology/CourseContent.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+namespace aptXR.Microbiology
+{
+    [CreateAssetMenu(fileName = "CourseContent", menuName = "MicroBiology/CourseContent")]
+    public class CourseContent: ScriptableObject
+    {
+
+        public string CourseName;
+
+        [Header("Control Type")]
+        public bool needHands = true;
+        public bool needControllers = true;
+
+        [Header("The Scene")]
+        public string SceneName;
+
+        public float courseDuration = 20;
+        public bool courseCompleted = false;
+        public int courseXP = 15;
+        public bool isOpen = true;
+    }
+}
--- Assets/Scripts/Microbiology/levelManager.cs
+++ Assets/Scripts/Microbiology/levelManager.cs
@@ -11,7 +11,8 @@ namespace aptXR.Microbiology
         [SerializeField] private SceneNames sceneNames;
 
 
-
+        public float frequencyVibrat = 0.09f;
+        public float amplitudeVibrat =  0.01f;
 
         private SceneLoader sceneLoader;
 
@@ -29,6 +30,14 @@ namespace aptXR.Microbiology
         public void LoadMasterMenu()
         {
             sceneLoader.Load(sceneNames.AllScenes[1]);
+        }
+
+
+        // send little vibration
+        public void slideHaptic()
+        {
+            OVRInput.SetControllerVibration(frequencyVibrat, amplitudeVibrat, OVRInput.Controller.Active);
+           
         }
 
     }
